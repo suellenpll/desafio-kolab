2da61f1b46c768d14d502880a9812c45cc5b6615
suellenpll <suellenpll@gmail.com>
2018-05-06 16:25:00 -0300

    :sparkles: Added views to delete blog post inside the site.

--- dksite/blog/views.py
+++ dksite/blog/views.py
@@ -41,3 +41,8 @@ def post_edit(request, pk):
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
