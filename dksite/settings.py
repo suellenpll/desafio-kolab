3edf88a239cdc56ee04b81946bad66374ffb807c
suellenpll <suellenpll@gmail.com>
2018-05-06 19:13:28 -0300

    :rocket: Setting Heroku requirements to deploy.

--- dksite/settings.py
+++ dksite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = 'omgbs%fakl2wz$j!1708i-!#^uj44(z2*f)0)68t#bg7qqj3ud'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'suellenpll.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'dksite.herokuapp.com']
 
 
 # Application definition
@@ -91,6 +92,9 @@ DATABASES = {
         'PORT': '',
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
